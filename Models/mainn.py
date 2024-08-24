88d36d51e255e179ac9309a566e1040ee7fb21ab
Gourav Thakur <gtgouravth2003@gmail.com>
2024-08-25 04:18:36 +0530

    total people

--- Models/mainn.py
+++ Models/mainn.py
@@ -7,7 +7,6 @@ from ultralytics import YOLO
 # Initialize webcam
 # cap = cv2.VideoCapture(0)
 
-model = YOLO('yolov8n.pt')  # You can replace with 'yolov8s.pt', 'yolov8m.pt', etc.
 # gender_net = cv2.dnn.readNetFromCaffe('deploy_gender.prototxt', 'gender_net.caffemodel')
 # GENDER_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 # GENDER_LIST = ['Male', 'Female']
@@ -16,8 +15,8 @@ def riskCalc(frame_path, model):
     parameters = [0, 0, 0, 0, 0.4] # total people, male, female, is_night, location index
     # if not ret:
     #     break
-    frame = cv2.imread(frame_path)
-
+    cap = cv2.VideoCapture(frame_path)
+    ret, frame = cap.read()
     # Perform object detection
     results = model(frame)
 
@@ -48,7 +47,7 @@ def riskCalc(frame_path, model):
     cv2.putText(annotated_frame, f"Total Objects: {total_objects}", (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
     # Display the resulting frame
-    cv2.imshow('YOLOv8 Live Tracking with Counts', frame)
+    cv2.imshow('YOLOv8 Live Tracking with Counts', annotated_frame)
     for class_name, count in counts.items():
         if class_name == 'person':
             pers=count
@@ -60,22 +59,23 @@ def riskCalc(frame_path, model):
 
     return parameters
 
-# print(riskCalc('./sam.jpg', model))
-path = r'womenSafetyAnalytics/Models/sam.jpg'
+model = YOLO('yolov8n.pt')  # You can replace with 'yolov8s.pt', 'yolov8m.pt', etc.
+print(riskCalc('./sam.jpg', model))
+# path = r'womenSafetyAnalytics/Models/sam.jpg'
 
-# Reading an image in default mode
-image = cv2.imread(path)
+# # Reading an image in default mode
+# image = cv2.imread(path)
 
-# Window name in which image is displayed
-window_name = 'image'
+# # Window name in which image is displayed
+# window_name = 'image'
 
-# Using cv2.imshow() method
-# Displaying the image
-cv2.imshow(window_name, image)
+# # Using cv2.imshow() method
+# # Displaying the image
+# cv2.imshow(window_name, image)
 
-# waits for user to press any key
-# (this is necessary to avoid Python kernel form crashing)
-cv2.waitKey(0)
+# # waits for user to press any key
+# # (this is necessary to avoid Python kernel form crashing)
+# cv2.waitKey(0)
 
-# closing all open windows
-cv2.destroyAllWindows()
+# # closing all open windows
+# cv2.destroyAllWindows()
--- Models/test1.py
+++ Models/test1.py
@@ -7,8 +7,9 @@ model = YOLO('yolov8n.pt')  # You can replace with 'yolov8s.pt', 'yolov8m.pt', e
 
 # Initialize webcam
 cap = cv2.VideoCapture(0)  # Use 0 for default webcam, or specify the camera index
-
+counter = 0
 while True:
+    counter+=1
     ret, frame = cap.read()  # Capture frame-by-frame
     if not ret:
         break
@@ -33,19 +34,31 @@ while True:
 
     # Display counts on the frame
     y_offset = 30
+    pers = 0
     for class_name, count in counts.items():
-        cv2.putText(annotated_frame, f"{class_name}: {count}", (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        if class_name == 'person':
+            pers=count
+        # cv2.putText(annotated_frame, f"{class_name}: {count}", (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         y_offset += 30
 
     # Display total objects count
     total_objects = sum(counts.values())
-    cv2.putText(annotated_frame, f"Total Objects: {total_objects}", (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    # cv2.putText(annotated_frame, f"Total Objects: {total_objects}", (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
     # Display the resulting frame
-    cv2.imshow('YOLOv8 Live Tracking with Counts', annotated_frame)
+    # cv2.imshow('YOLOv8 Live Tracking with Counts', annotated_frame)
 
     # Break the loop on 'q' key press
+    if True:
+        parameters = [0, 0, 0, 0, 0.4]
+        parameters[0] = pers
+        parameters[1] = pers//2
+        parameters[2] = pers//2
+        print(parameters)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    ca = int(input("Capture frame? (1/0) - "))
+    if ca==0:
         break
 
 # Release the capture and close windows

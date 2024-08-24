76080e48a6b0ac49a9eff9f3eabc7efc4dd06ba0
sen-Saptarshi <113275417+sen-Saptarshi@users.noreply.github.com>
2024-08-25 04:19:43 +0530

    okk

--- Models/mainn.py
+++ Models/mainn.py
@@ -48,7 +48,7 @@ def riskCalc(frame_path, model):
     cv2.putText(annotated_frame, f"Total Objects: {total_objects}", (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
     # Display the resulting frame
-    cv2.imshow('YOLOv8 Live Tracking with Counts', frame)
+    # cv2.imshow('YOLOv8 Live Tracking with Counts', frame)
     for class_name, count in counts.items():
         if class_name == 'person':
             pers=count
@@ -60,22 +60,22 @@ def riskCalc(frame_path, model):
 
     return parameters
 
-# print(riskCalc('./sam.jpg', model))
-path = r'womenSafetyAnalytics/Models/sam.jpg'
+print(riskCalc('./sam.jpg', model))
+# path = r'sam.jpg'
 
 # Reading an image in default mode
-image = cv2.imread(path)
+# image = cv2.imread(path)
 
-# Window name in which image is displayed
-window_name = 'image'
+# # Window name in which image is displayed
+# window_name = 'image'
 
-# Using cv2.imshow() method
-# Displaying the image
-cv2.imshow(window_name, image)
+# # Using cv2.imshow() method
+# # Displaying the image
+# cv2.imshow(window_name, image)
 
-# waits for user to press any key
-# (this is necessary to avoid Python kernel form crashing)
-cv2.waitKey(0)
+# # waits for user to press any key
+# # (this is necessary to avoid Python kernel form crashing)
+# cv2.waitKey(0)
 
-# closing all open windows
-cv2.destroyAllWindows()
+# # closing all open windows
+# cv2.destroyAllWindows()
